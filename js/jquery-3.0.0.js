c8d4e751af52614485f71b7b9f110976f54c80fd
fidel omondi <willgenius@gmail.com>
2019-03-04 17:10:44 +0300

    add a README

--- js/jquery-3.0.0.js
+++ js/jquery-3.0.0.js
@@ -1,29 +1,11 @@
-/*!
- * jQuery JavaScript Library v3.0.0
- * https://jquery.com/
- *
- * Includes Sizzle.js
- * https://sizzlejs.com/
- *
- * Copyright jQuery Foundation and other contributors
- * Released under the MIT license
- * https://jquery.org/license
- *
- * Date: 2016-06-09T18:02Z
- */
+
 ( function( global, factory ) {
 
 	"use strict";
 
 	if ( typeof module === "object" && typeof module.exports === "object" ) {
 
-		// For CommonJS and CommonJS-like environments where a proper `window`
-		// is present, execute the factory and get jQuery.
-		// For environments that do not have a `window` with a `document`
-		// (such as Node.js), expose a factory as module.exports.
-		// This accentuates the need for the creation of a real `window`.
-		// e.g. var jQuery = require("jquery")(window);
-		// See ticket #14549 for more info.
+		/ for more info.
 		module.exports = global.document ?
 			factory( global, true ) :
 			function( w ) {
